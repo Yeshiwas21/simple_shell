3ce848b10850c1777016163779c5e5b9533f0aa6
Yeshiwas21 <yeshiwasdagnaw23@gmail.com>
2023-04-27 15:09:08 -0700

    Revert "Project files"
    This reverts commit abf77cf237864cc4070f838458592841bb1a6c73.

--- string1.c
+++ string1.c
@@ -66,22 +66,22 @@ void _puts(char *str)
 
 /**
  * _putchar - writes the character c to stdout
- * @chr: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char chr)
+int _putchar(char c)
 {
 	static int i;
-	static char buff[WRITE_BUF_SIZE];
+	static char buf[WRITE_BUF_SIZE];
 
-	if (chr == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(1, buff, i);
+		write(1, buf, i);
 		i = 0;
 	}
-	if (chr != BUF_FLUSH)
-		buff[i++] = chr;
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
 	return (1);
 }
