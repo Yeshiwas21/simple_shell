16dc71940f35955f6d7cd1961dc9dbc6a22b6722
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-04-27 23:51:29 +0300

    Project files

--- getLine.c
+++ getLine.c
@@ -3,7 +3,7 @@
 /**
  * input_buf - buffers chained commands
  * @info: parameter struct
- * @buf: address of buffer
+ * @buff: address of buffer
  * @len: address of len var
  *
  * Return: bytes read
