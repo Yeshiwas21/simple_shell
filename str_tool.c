0de4d98a10457b1c63c2d47c06d3b88e2a6b0dc4
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-05-24 15:57:38 +0300

    Updated

--- str_tool.c
+++ str_tool.c
@@ -1,104 +1,61 @@
 #include <stdio.h>
 #include "shell.h"
-/**
-* _strncmp - compares two strings up to n bytes
-* @s1: compared to s2
-* @s2: compared to s1
-* @n: number of bytes
-*
-* Return: difference between s1 and s2
-*/
-int _strncmp(char *s1, char *s2, int n)
-{
-	int i;
 
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+int _strncmp(const char *s1, const char *s2, int n)
+{
+    int i;
+    
+    for (i = 0; i < n && s1[i] && s2[i] && s1[i] == s2[i]; i++);
+    
+    if (i == n)
+        return 0;
+    else
+        return s1[i] - s2[i];
 }
-/**
- * _strlen -  returns the length of a string
- * @s:string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int longi = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	return (longi);
+int _strlen(const char *s)
+{
+    int length = 0;
+    
+    while (*s++)
+        length++;
+    
+    return length;
 }
-/**
- * *_strcpy - copies the string pointed by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: pointer  in buffer to be copied
- * @src: string to be copied
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
+
+char *_strcpy(char *dest, const char *src)
 {
-	int len, i;
-
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
+    char *dest_start = dest;
+    
+    while (*src)
+        *dest++ = *src++;
+        
+    *dest = '\0';
+    
+    return dest_start;
 }
-/**
- * _strcmp - compares two strings
- * @s1: pointer to first string
- * @s2: pointer to second string
- * Return: if str1 < str2, negative difference is unmatched char
- * if str1 == str2, 0
- * if str1 > str2, positive difference is unmatched char
- */
-int _strcmp(char *s1, char *s2)
+
+int _strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+    while (*s1 && *s2 && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    
+    return *s1 - *s2;
 }
-/**
- *  *_strcat- concatinate two strings
- *  @dest: first string
- *  @src: second string
- *  Return: pointer
- */
-char  *_strcat(char *dest, char *src)
+
+char *_strcat(char *dest, const char *src)
 {
-	int r = 0;
-
-	int s = 0;
-
-	while (dest[r] != '\0')
-	{
-		r++;
-	}
-	while (src[s] != '\0')
-	{
-		dest[r] = src[s];
-		r++;
-		s++;
-	}
-	dest[r] = '\0';
-	return (dest);
+    char *dest_start = dest;
+    
+    while (*dest)
+        dest++;
+    
+    while (*src)
+        *dest++ = *src++;
+        
+    *dest = '\0';
+    
+    return dest_start;
 }
